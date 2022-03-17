f5721fa50a08efa747dead06b9bad23012d03234
Kindeme KOUNOUDJI <fkounoudji@gmail.com>
2022-03-17 00:40:06 -0600

    navbar fixed

--- js/about.js
+++ js/about.js
@@ -3,7 +3,15 @@ const $ = (selector) => document.querySelector(selector);
 
 const navToggle = $(".nav-toggle");
 const links = $(".links");
+const icon = $("#icon");
 
 navToggle.addEventListener("click", () => {
 	links.classList.toggle("show-links");
+	if (icon.classList.contains("fa-bars")) {
+		icon.classList.remove("fa-bars");
+		icon.classList.add("fa-times");
+	} else {
+		icon.classList.add("fa-bars");
+		icon.classList.remove("fa-times");
+	}
 });
--- js/service.js
+++ js/service.js
@@ -6,4 +6,11 @@ const links = $(".links");
 
 navToggle.addEventListener("click", () => {
 	links.classList.toggle("show-links");
+	if (icon.classList.contains("fa-bars")) {
+		icon.classList.remove("fa-bars");
+		icon.classList.add("fa-times");
+	} else {
+		icon.classList.add("fa-bars");
+		icon.classList.remove("fa-times");
+	}
 });
